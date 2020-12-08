dcb864437821a6f615f2ec6220184e4adff04891
Michelle <48816865+mtaylor62831@users.noreply.github.com>
2020-12-08 09:30:19 -0800

    Added button functionality and updated formatting.

---
+++ index.js
@@ -0,0 +1,11 @@
+document.getElementById("aboutBtn").addEventListener("click", toAbout);
+
+function toAbout() {
+    window.location.href = "/aboutMe.html";
+}
+
+document.getElementById("portfolioBtn").addEventListener("click", toAbout);
+
+function toAbout() {
+    window.location.href = "/portfolio.html";
+}
